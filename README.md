dc821ba164c565593a71461b3f3938093f04162d
nguarino522 <sub4miler5@gmail.com>
2022-08-15 13:21:22 -0400

    project complete finished localStorage utilization

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Code for image in title: https://icon-library.com/icon/todo-icon-16.html.html>To
 You can browse to the GitHub pages website if you want to test it out or download project files and load the 'index.html' file in a web browser.
 
 Public link: https://nguarino522.github.io/todo_app/
+
+With utilizing localStorage, you can pick up where left off as long as localStorage isn't cleared.
--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-let todoForm = document.getElementById("todoform");
-let taskContainer = document.getElementById("taskcontain");
+const todoForm = document.getElementById("todoform");
+const taskContainer = document.getElementById("taskcontain");
+
+// check and retrieve anything from localStorage and create it
+const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+console.log(savedTasks[1]);
+for (let i = 0; i < savedTasks.length; i++) {
+    
+    let newTask = document.createElement("span");
+    newTask.classList.add("card", "card-1");
+    newTask.isComplete = savedTasks[i].isComplete ? true : false;
+
+    let taskContent = document.createElement("div");
+    taskContent.classList.add("card-content");
+
+    let taskName = document.createElement("span")
+    taskName.classList.add("card-title");
+    taskName.innerText = savedTasks[i].task;
+    console.log(savedTasks[i].task);
+
+    let cardAction = document.createElement("div");
+    cardAction.classList.add("card-action");
+
+    let completeBtn = document.createElement("a");
+    completeBtn.classList.add("waves-effect", "waves-light", "btn", "green", "lighten-1");
+    completeBtn.innerText = "Complete";
+    completeBtn.id = "completebtn";
+
+    let removeBtn = document.createElement("a");
+    removeBtn.classList.add("waves-effect", "waves-light", "btn", "red", "lighten-1");
+    removeBtn.innerText = "Remove";
+    removeBtn.id = "removebtn";
+
+    cardAction.appendChild(completeBtn);
+    cardAction.appendChild(removeBtn);
+    taskContent.appendChild(taskName);
+    newTask.appendChild(taskContent);
+    newTask.appendChild(cardAction);
+    taskContainer.appendChild(newTask);
+
+    if (newTask.isComplete === true){
+        newTask.style.textDecoration = "line-through";
+        newTask.style.backgroundColor = "#90EE90";
+    }
+
+};
+
 
 todoForm.addEventListener("submit", function(evt) {
     evt.preventDefault();
@@ -7,6 +52,7 @@ todoForm.addEventListener("submit", function(evt) {
     // creating all card/box items for new task
     let newTask = document.createElement("span");
     newTask.classList.add("card", "card-1");
+    newTask.isComplete = false;
 
     let taskContent = document.createElement("div");
     taskContent.classList.add("card-content");
@@ -36,18 +82,44 @@ todoForm.addEventListener("submit", function(evt) {
     newTask.appendChild(cardAction);
     taskContainer.appendChild(newTask);
 
+    //reset form
     todoForm.reset();
+
+    // save to localStorage
+    savedTasks.push({task: taskName.innerText, isComplete: false});
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+
 });
 
 taskContainer.addEventListener("click", function(evt) {
+
     if (evt.target.id === "removebtn") {
         evt.target.parentElement.parentElement.remove();
+        for (let i=0; i < savedTasks.length; i++) {
+            console.log(i);
+            if (savedTasks[i].task === evt.target.parentElement.parentElement.firstChild.innerText) {
+                savedTasks.splice(i,1);
+                localStorage.setItem("tasks", JSON.stringify(savedTasks));   
+            }
+        }
+
     };
 
     if (evt.target.id === "completebtn") {
         let task = evt.target.parentElement.parentElement;
         evt.target.parentElement.parentElement.style.textDecoration = "line-through";
         evt.target.parentElement.parentElement.style.backgroundColor = "#90EE90";
+        evt.target.parentElement.parentElement.isComplete = true;
+
+        for (let i=0; i < savedTasks.length; i++) {
+            if (savedTasks[i].task == evt.target.parentElement.parentElement.firstChild.innerText) {
+                savedTasks[i].isComplete = !savedTasks[i].isComplete;
+                localStorage.setItem("tasks", JSON.stringify(savedTasks));
+            }
+        }
+
     };
+
+
 
 });
